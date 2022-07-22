639b0cfb6c39ccde7591bc654e9f6c191921ed89
Melanie Beck <mbeck@cloudera.com>
2022-07-22 19:20:21 +0000

    fix dashboard address and port

--- utils/dask_utils.py
+++ utils/dask_utils.py
@@ -1,14 +1,57 @@
-import os, subprocess, socket, time
+# ###########################################################################
+#
+#  CLOUDERA APPLIED MACHINE LEARNING PROTOTYPE (AMP)
+#  (C) Cloudera, Inc. 2021
+#  All rights reserved.
+#
+#  Applicable Open Source License: Apache 2.0
+#
+#  NOTE: Cloudera open source products are modular software products 
+#  made up of hundreds of individual components, each of which was 
+#  individually copyrighted.  Each Cloudera open source product is a 
+#  collective work under U.S. Copyright Law. Your license to use the 
+#  collective work is as provided in your written agreement with  
+#  Cloudera.  Used apart from the collective work, this file is 
+#  licensed for your use pursuant to the open source license 
+#  identified above.
+#
+#  This code is provided to you pursuant a written agreement with
+#  (i) Cloudera, Inc. or (ii) a third-party authorized to distribute 
+#  this code. If you do not have a written agreement with Cloudera nor 
+#  with an authorized and properly licensed third party, you do not 
+#  have any rights to access nor to use this code.
+#
+#  Absent a written agreement with Cloudera, Inc. (“Cloudera”) to the
+#  contrary, A) CLOUDERA PROVIDES THIS CODE TO YOU WITHOUT WARRANTIES OF ANY
+#  KIND; (B) CLOUDERA DISCLAIMS ANY AND ALL EXPRESS AND IMPLIED 
+#  WARRANTIES WITH RESPECT TO THIS CODE, INCLUDING BUT NOT LIMITED TO 
+#  IMPLIED WARRANTIES OF TITLE, NON-INFRINGEMENT, MERCHANTABILITY AND 
+#  FITNESS FOR A PARTICULAR PURPOSE; (C) CLOUDERA IS NOT LIABLE TO YOU, 
+#  AND WILL NOT DEFEND, INDEMNIFY, NOR HOLD YOU HARMLESS FOR ANY CLAIMS 
+#  ARISING FROM OR RELATED TO THE CODE; AND (D)WITH RESPECT TO YOUR EXERCISE 
+#  OF ANY RIGHTS GRANTED TO YOU FOR THE CODE, CLOUDERA IS NOT LIABLE FOR ANY
+#  DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, PUNITIVE OR
+#  CONSEQUENTIAL DAMAGES INCLUDING, BUT NOT LIMITED TO, DAMAGES 
+#  RELATED TO LOST REVENUE, LOST PROFITS, LOSS OF INCOME, LOSS OF 
+#  BUSINESS ADVANTAGE OR UNAVAILABILITY, OR LOSS OR CORRUPTION OF
+#  DATA.
+#
+# ###########################################################################
+
+import os
 import cdsw
 
+# This default may not work in some envs - change to whatever is appropriate 
+DASHBOARD_PORT = os.environ['CDSW_READONLY_PORT']
 
-def run_scheduler(num_workers=1, cpu=1, memory=2):
+
+def run_scheduler(dashboard_port, num_workers=1, cpu=1, memory=2):
     """
     Run a Dask Scheduler process in a CDSW worker.
     """
     scheduler_code = f"""!dask-scheduler \
         --host 0.0.0.0 \
-        --dashboard-address 127.0.0.1:{os.environ['CDSW_READONLY_PORT']}"""
+        --dashboard-address 127.0.0.1:{dashboard_port}""" 
     dask_scheduler = cdsw.launch_workers(
         n=num_workers,
         cpu=cpu,
@@ -38,6 +81,11 @@ def get_scheduler_url(dask_scheduler):
     ][0]
 
     return f"tcp://{scheduler_ip}:8786"
+
+
+def get_dashboard_url(dask_scheduler):
+    """ Given a Dask Scheduler, return the Dask Dashboard url"""
+    return("//".join(dask_scheduler[0]["app_url"].split("//")) + "status")
   
 
 def run_dask_workers(scheduler_url, num_workers, cpu, memory, nvidia_gpu=0):
@@ -61,13 +109,14 @@ def run_dask_workers(scheduler_url, num_workers, cpu, memory, nvidia_gpu=0):
     return workers
 
 
-def run_dask_cluster(num_workers, cpu, memory, nvidia_gpu=0):
+def run_dask_cluster(num_workers, cpu, memory, nvidia_gpu=0, dashboard_port=DASHBOARD_PORT):
     """
     Runs a Dask Scheduler and the requested number of Dask Workers
     as CDSW workers via the Workers API. 
     """
-    dask_scheduler = run_scheduler()
+    dask_scheduler = run_scheduler(dashboard_port=dashboard_port)
     scheduler_url = get_scheduler_url(dask_scheduler)
+    dashboard_url = get_dashboard_url(dask_scheduler)
     dask_workers = run_dask_workers(
         scheduler_url=scheduler_url,
         num_workers=num_workers, 
@@ -78,5 +127,6 @@ def run_dask_cluster(num_workers, cpu, memory, nvidia_gpu=0):
     return {
         "scheduler": dask_scheduler,
         "workers": dask_workers,
-        "scheduler_address": scheduler_url
+        "scheduler_address": scheduler_url,
+        "dashboard_address": dashboard_url
         }
